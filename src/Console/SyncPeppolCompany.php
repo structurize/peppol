927585f63ee668d44c7173388e233509485b9649
Ashim De Coster <ashim@deltasolutions.be>
2025-12-01 15:00:46 +0100

    config without helper

--- src/Console/SyncPeppolCompany.php
+++ src/Console/SyncPeppolCompany.php
@@ -3,6 +3,7 @@
 namespace Structurize\Peppol\Console;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Config;
 use Structurize\Peppol\Models\Company;
 use Structurize\Peppol\Services\PeppolService;
 
@@ -10,6 +11,7 @@ class SyncPeppolCompany extends Command
 {
     const SUCCESS = 1;
     protected $signature = 'peppol:sync-companies';
+    protected $name = 'peppol:sync-companies';
     protected $description = 'This command synchronizes companies connected with Peppol';
 
     public function handle(PeppolService $svc)
@@ -28,13 +30,13 @@ class SyncPeppolCompany extends Command
 
     private function checkCompanies()
     {
-        $vat_number = config('peppol.table-fields.companies.vat_number', 'vat_number');
-        $company_id = config('peppol.table-fields.companies.id', 'id');
+        $vat_number = Config::get('peppol.table-fields.companies.vat_number', 'vat_number');
+        $company_id = Config::get('peppol.table-fields.companies.id', 'id');
 
         $companies = Company::whereNotNull($vat_number)->get();
         $peppolService = app(PeppolService::class);
 
-        $_ENV['STRUCTURIZE_API_KEY'] = config('peppol.api_key');
+        $_ENV['STRUCTURIZE_API_KEY'] = Config::get('peppol.api_key');
 
         foreach ($companies as $company) {
             try {
